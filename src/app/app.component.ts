6f22505417c5760319fb356268d40f1fa19ef572
Vinicius Santos <vinicius.santos110vs@gmail.com>
2023-11-25 16:35:32 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RegistroComponent } from "./registro/registro.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, RegistroComponent]
 })
 export class AppComponent {
   title = 'cuidar';
---
+++ src/app/registro/registro.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-registro',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './registro.component.html',
+  styleUrl: './registro.component.css'
+})
+export class RegistroComponent {
+
+}
